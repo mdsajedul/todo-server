7c69c6c7176e349d862db6b1a107c6aff41c4326
mdsajedul <sajedulislms@gmail.com>
2022-03-11 02:41:18 +0600

    signup of user

--- route-handler/userHandler.js
+++ route-handler/userHandler.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Model/userModel');
+const bcrypt = require('bcrypt');
 
 
-router.post('/signup',(req,res)=>{
-    
+router.post('/signup',async (req,res)=>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = new User({
+            name:req.body.name,
+            username:req.body.username,
+            password: hashedPassword
+        });
+        await newUser.save();
+        res.status(200).json({message:'Signup Successful'})
+    }
+    catch{
+        res.status(500).json({error:'Signup was not successful'});
+    }
 })
 
 
